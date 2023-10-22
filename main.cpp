c6be919fde5a4e1ce6ae2db5962b0df5c61fa682
Your Name <you@example.com>
2023-10-22 12:00:03 +0300

    Add function to calcuate average of numbers

--- main.cpp
+++ main.cpp
@@ -11,8 +11,17 @@ int getSum(vector<int>nums) {
 
     return sum;
 }
+
+double getAverage(vector<int>nums) {
+
+    int sum = getSum(nums);
+    int size = nums.size();
+    
+    return (double)sum / size;
+}
 int main() {
     
     cout<<"Sum = "<<getSum({1,2,3,4,5,6,7,8,9,10})<<'\n';
+    cout<<"Average = "<<getAverage({1,2,3,4,5,6,7,8,9,10})<<'\n';
         
 }
