9f763e37c2887dc45d48529efeaf1cdb36e666a4
Your Name <you@example.com>
2023-10-22 12:10:52 +0300

    Add function to get Minimum of numbers

--- main.cpp
+++ main.cpp
@@ -11,8 +11,22 @@ int getSum(vector<int>nums) {
 
     return sum;
 }
+int getMin(vector<int>nums) {
+
+    int minElement = INT32_MAX;
+
+    for(auto num : nums) {
+
+        if(minElement > num) {
+            minElement = num;
+        }
+    }
+
+    return minElement;
+}
 int main() {
     
     cout<<"Sum = "<<getSum({1,2,3,4,5,6,7,8,9,10})<<'\n';
+    cout<<"Min Element  = "<<getMin({-10,-2,-8,10,8,15,1,2,3,-100})<<'\n';
         
 }
